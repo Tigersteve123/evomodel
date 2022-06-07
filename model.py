c551b1d2883167251c28effed112309b231a5e87
Yichen Sun <50383075+Tigersteve123@users.noreply.github.com>
2022-06-07 18:53:38 -0400

    Commit all changes

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import numpy as np
 import math
-import matplotlib.pyplot as plt
 
 class model:
 	def __init__(self, b0:float, db:float, nb:int, xi0:float, dxi:float, nxi:int, ps:float):
@@ -93,31 +92,3 @@ class model:
 		#print(lst1)
 		return lst1, lst2, lstI, lstS
 
-#np.random.seed(0)
-
-#mod = model(.001, .001, 3, 0, 0, 1, 1) #"base" Reed-Frost
-
-mod = model(.001, .001, 10, 0.5, 0.001, 5, .5)
-lst1, lst2, lstI, lstS = mod.sim((0, 0), 60, 1500)
-lstI_sep = [lst1[x]+lst2[x] for x in range(len(lst1))]
-for i in lstI_sep:
-	#plt.imshow(i, cmap='gray', vmin=0, vmax=255)
-	plt.matshow(i)
-	plt.show()
-#print(lst1)
-#print(lst2)
-
-runs_I = []
-runs_S = []
-'''for i in range(100):
-	lst1, lst2, lstI, lstS = mod.sim((0, 0), 60, 1500)
-	runs_I.append(lstI)
-	runs_S.append(lstS)
-#plt.hist([max([i[0] for i in x]) for x in runs])
-average_dis = [sum(x)/len(x) for x in zip(*runs_I)]
-std_dis = [np.std(x) for x in zip(*runs_I)]
-print(average_dis)
-print(std_dis)
-plt.plot([x for x in range(len(average_dis))], average_dis)
-plt.plot([x for x in range(len(std_dis))], std_dis)
-plt.show()'''
