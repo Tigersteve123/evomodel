352307c4ae82052b0773eea129035006057ed204
Yichen Sun <50383075+Tigersteve123@users.noreply.github.com>
2022-06-18 20:55:40 -0400

    Update main2.py

--- main2.py
+++ main2.py
@@ -10,32 +10,15 @@ import concurrent.futures as futures
 
 #mod = model(.001, .001, 3, 0, 0, 1, 1) #"base" Reed-Frost
 
-#mod = model(.001, .001, 10, 0.5, 0.001, 5, .5) #initial test case
-#i0 = np.zeros((len(mod.brange), len(mod.xirange)), dtype=int)
-#i0[0, 0] = 60
 if __name__ == '__main__':
 	mod = model(0.0006000000000000001, .0001, 10, 0.1, 0.001, 5, .6)
 	total = np.zeros((1, len(mod.brange), len(mod.xirange)), dtype=int)
 	total1 = total.copy()
 	total2 = total.copy()
-	#print(total)
-	#i0_uniform = np.ones((len(mod.brange), len(mod.xirange)), dtype=int)
-	#i0_worstcorner = np.zeros((len(mod.brange), len(mod.xirange)), dtype=int)
-	#i0_bestcorner = i0_worstcorner.copy()
 	i0_center = np.zeros((len(mod.brange), len(mod.xirange)), dtype=int)
-	#i0_worstcorner[0, 0] = 50
-	#i0_bestcorner[9, 4] = 50
 	i0_center[5, 2] = 50
-	#print(i0_center)
 	for i in range(1000):
-		#print("Run", i)
-		#i0 = np.ones((len(mod.brange), len(mod.xirange)), dtype=int)
-		#i0[5, 2] = 500
 		lst1, lst2, lstI, lstS = mod.sim(1500, i0_center.copy())
-		#lstI_sep = [lst1[x]+lst2[x] for x in range(len(lst1))]
-		#print(np.sum(lst1))
-		#print(lstI)
-		#print(lstS)
 		for x in range(len(lst1)):
 			try:
 				total[x] = total[x]+lst1[x]+lst2[x]
@@ -46,8 +29,6 @@ if __name__ == '__main__':
 				total1 = np.concatenate((total1, [lst1[x]]))
 				total2 = np.concatenate((total2, [lst2[x]]))
 	print(np.sum(total), np.sum(total1), np.sum(total2))
-			#print(total)
-			#print("=================")
 	#filename = 'runs/test_'+str(b0)+'_'+str(xi0)+'_'+str(ps)+'.npy'
 	with open('test1.npy', 'wb') as f:
 		np.save(f, total)
@@ -60,23 +41,3 @@ if __name__ == '__main__':
 	#	plt.show()
 	#print(lst1)
 	#print(lst2)
-
-'''mod = model(.0005, .0001, 10, 0.5, 0.001, 5, .5)
-i0 = np.ones((len(mod.brange), len(mod.xirange)), dtype=int) #test case
-print(mod.sim(1500, i0))'''
-
-
-'''runs_I = []
-runs_S = []
-for i in range(100):
-	lst1, lst2, lstI, lstS = mod.sim((0, 0), 60, 1500)
-	runs_I.append(lstI)
-	runs_S.append(lstS)
-#plt.hist([max([i[0] for i in x]) for x in runs])
-average_dis = [sum(x)/len(x) for x in zip(*runs_I)]
-std_dis = [np.std(x) for x in zip(*runs_I)]
-print(average_dis)
-print(std_dis)
-plt.plot([x for x in range(len(average_dis))], average_dis)
-plt.plot([x for x in range(len(std_dis))], std_dis)
-plt.show()'''
