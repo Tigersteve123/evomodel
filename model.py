5687a235f8a9b12f814751e038d8babbb332678c
Yichen Sun <50383075+Tigersteve123@users.noreply.github.com>
2022-08-14 16:39:37 -0400

    Update model with new code base

--- model.py
+++ model.py
@@ -25,7 +25,7 @@ class model:
 		return [x for x in neighbors if (x[0] in range(len(self.brange)) and x[1] in range(len(self.grange)))]
 
 	def sim(self, s:int, i0:np.ndarray, tc=0, acc=0):
-		lst1 = [] #history of infected
+		'''lst1 = [] #history of infected
 		lst2 = lst1.copy()
 		lstS = [s]
 		lstI = [np.sum(i0)]
@@ -134,5 +134,77 @@ class model:
 			lstQS.append(QS)
 			lstQI1.append(QI)
 			lstQI2.append(QI2)
+			t += 1'''
+		lstS = [s]
+		lstIhat = [np.sum(i0)]
+		lstI1 = [i0.copy()]
+		lstI2 = [np.zeros((len(self.brange), len(self.grange)), dtype=int)]
+		lstQS = [0]
+		lstQI1 = [0]
+		lstQI2 = [np.zeros((len(self.brange), len(self.grange)), dtype=int)]
+		t = 0
+		
+		Ihat2 = np.zeros((len(self.brange), len(self.grange)), dtype=int)
+		p = np.zeros((len(self.brange), len(self.grange)), dtype=float)
+		Ibar = np.zeros((len(self.brange), len(self.grange)), dtype=int)
+		st = np.zeros((len(self.brange), len(self.grange)), dtype=int)
+		sbar = np.zeros((len(self.brange), len(self.grange)), dtype=int)
+		I1 = i0.copy()
+		I2 = np.zeros((len(self.brange), len(self.grange)), dtype=int)
+		QI2 = I2.copy()
+		
+		while np.sum(I1)+np.sum(I2) > 0.0001 and lstS[-1] > 0.0001:
+			#print(np.sum(I1)+np.sum(I2), lstS[-1])
+			I1_lastPeriod = lstI1[-1]
+			I2_lastPeriod = lstI2[-1]
+			ITotal_lastPeriod = I1_lastPeriod+I2_lastPeriod
+			S_lastPeriod = lstS[-1]
+			
+			### Infection ###
+			for i in range(len(self.brange)):
+				for j in range(len(self.grange)):
+					Ihat2[i, j] = np.random.binomial(I1_lastPeriod[i, j], self.grange[j])
+			Ihat = np.random.binomial(S_lastPeriod, (1-math.prod( (np.subtract(1, self.brange))**(np.sum(I1_lastPeriod, 1)+np.sum(I2_lastPeriod, 1)) )))
+			Shat = S_lastPeriod-Ihat
+			
+			tau = min(C/(S_lastPeriod+np.sum(Ihat2)), 1)
+			
+			QS = np.random.binomial(Shat, tau*(1-alpha))
+			try: QS2 = lstQS[-2]
+			except: QS2 = 0
+			S = Shat-QS+QS2
+			
+			QI = np.random.binomial(Ihat, tau*alpha)
+			I = Ihat-QI
+			for i in range(len(self.brange)):
+				for j in range(len(self.grange)):
+					QI2[i, j] = np.random.binomial(Ihat2[i, j], tau*alpha)
+			I2 = Ihat2-QI2
+			
+			for i in range(len(self.brange)):
+				for j in range(len(self.grange)):
+					p[i, j] = self.brange[i]*(ITotal_lastPeriod[i, j])/np.sum(self.brange* np.sum(ITotal_lastPeriod, 1)) #calculate denominator separately for efficiency
+			Ibar = np.reshape(np.random.multinomial(I, p.flatten()), (len(self.brange), len(self.grange)))
+			st = Ibar[i, j]*self.ps #take out of loop
+			sbar = Ibar-st
+			I1 = st.copy()
+			
+			### Evolution ###
+			for i in range(len(self.brange)):
+				for j in range(len(self.grange)):
+					probs = [1/len(self.neighborcoords(i, j)) for a in range(len(self.neighborcoords(i, j)))]
+					neighborParray = np.random.multinomial(sbar[i, j], probs)
+					narray = self.neighborcoords(i, j)
+					for x in range(len(narray)):
+						I1[narray[x]] = I1[narray[x]]+neighborParray[x]
+			
+			### Data ###
+			lstS.append(S)
+			lstIhat.append(Ihat)
+			lstI1.append(I1)
+			lstI2.append(I2)
+			lstQS.append(QS)
+			lstQI1.append(QI)
+			lstQI2.append(QI2)
 			t += 1
-		return np.array(lst1), np.array(lst2), np.array(lstI), np.array(lstS), np.array(lstAvg), np.array(lstCumAvg), np.array(lstQS), np.array(lstQI1), np.array(lstQI2), t
+		return np.array(lstI1), np.array(lstI2), np.array(lstIhat), np.array(lstS), 0, 0, np.array(lstQS), np.array(lstQI1), np.array(lstQI2), t #appending 0 for backwards capacity
